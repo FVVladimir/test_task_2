19ba8f4aedbb798f4ccbfa629b6a7a1de4dbeded
Volodymyr Fedchenko <emirwtf@gmail.com>
2026-03-26 21:23:04 +0200

    added technical requirment

--- src/specs/test.spec.js
+++ src/specs/test.spec.js
@@ -1,13 +1,24 @@
-//https://www.saucedemo.com/
+// Technical Requirements:
+
+// Tool: WebDriverIO.
+
+// Browsers: Chrome, Edge (Run in Parallel).
+
+// Pattern: Page Object Model (POM).
+
+// Locators: CSS Selectors.
+
+// Reporting: Generate an Allure Report (or similar HTML report) for the test run.
+
+// Documentation: Add a README.md explaining how to run the tests and generate the report.
 
 describe("first test", ()=> {
        
     beforeEach(async ()=> {
         await browser.url("https://www.saucedemo.com/")
-    })
+    });
 
-    it("check title", async ()=> {
-        const title = await browser.getTitle();
-        console.log(title)
-    })
+    it("check title", async ()=> {        
+        await expect(browser).toHaveTitle("Swag Labs");
+    });
 })
