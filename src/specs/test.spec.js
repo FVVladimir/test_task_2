5ffc49c3ac0f528a9cc4171d4028f05efdaa88ae
Volodymyr Fedchenko <emirwtf@gmail.com>
2026-03-27 21:01:17 +0200

    add steps

--- src/specs/test.spec.js
+++ src/specs/test.spec.js
@@ -12,13 +12,48 @@
 
 // Documentation: Add a README.md explaining how to run the tests and generate the report.
 
-describe("first test", ()=> {
+
+
+describe("UC-1 Checkout Flow", ()=> {
        
     beforeEach(async ()=> {
         await browser.url("https://www.saucedemo.com/")
     });
 
-    it("check title", async ()=> {        
+    it("Login with standard_user", async ()=> {      
+        const window =  $(".login_wrapper-inner");
+        const username;
+        const passWord;
+        const loginButton;
         await expect(browser).toHaveTitle("Swag Labs");
+        await expect(window).toBeDisplayed();
     });
-})
+
+    it("Add a specific product to the cart (parametrize the product name, e.g., 'Sauce Labs Backpack'", async ()=> {
+        console.log("i am a second step")
+    });
+
+    it("Navigate to the Cart and validate the item is present", async ()=> {
+        console.log("i am a therd step")
+    });
+
+    it("Proceed to Checkout", async ()=> {
+        console.log("i am a fourth step")
+    });
+
+    it("Fill in the Information form (First Name, Last Name, Zip)", async ()=> {
+        console.log("i am a fifth step")
+    });
+
+    it("Complete the checkout and validate the success message: 'Thank you for your order!'", async ()=> {
+        console.log("i am a sixth step")
+    });
+});
+
+// UC-2 Data Driven Login:
+
+// Use a Data Provider to test login with:
+
+// 1. standard_user (Should pass).
+
+// 2. locked_out_user (Should fail with specific error message).
